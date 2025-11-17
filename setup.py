e51b66ab021fb6826181a981bad3d09725d3cd92
janisaiad <janisaiad.ja@gmail.com>
2025-11-17 16:59:23 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
+import tomli  # or tomllib in Python 3.11+
 
 if __name__ == "__main__":
-    setup()
+    with open("pyproject.toml", "rb") as f:
+        pyproject = tomli.load(f)
+    
+    setup(
+        name=pyproject["project"]["name"],
+        version=pyproject["project"]["version"],
+    )
